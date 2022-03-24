908c034eeb1bf4927fe6f2268a44bf8929701bcd
pacman766 <3334976@gmail.com>
2022-03-24 09:50:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.scss';
 
 function App() {
+  const [id, setId] = useState(0);
+  const [word, setWord] = useState('');
+  const [translate, settranslate] = useState('');
+  const [cards, setCards] = useState([]);
+
+  // const onChange = (e) => {
+  //   e.target.value
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Карточки англ слов</h1>
+      <div className="container">
+        <input placeholder='type in english' type="text" className="english" value={word} />
+        <input placeholder='введите на русском' type="text" className="russian" onChange={(e) => onChange('word', e.target.value)}/>
+      </div>
     </div>
   );
 }
